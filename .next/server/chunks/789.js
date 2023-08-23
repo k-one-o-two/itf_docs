115ae8a7ed02c8944cdb73c778bc33b18bcff49f
k-one-o-two <k.one.o.two@gmail.com>
2023-08-23 11:56:40 +0300

    build

---
+++ .next/server/chunks/789.js
@@ -0,0 +1,25 @@
+exports.id = 789;
+exports.ids = [789];
+exports.modules = {
+
+/***/ 966:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 2987, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 831, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6926, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 4282, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6505, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1232, 23))
+
+/***/ }),
+
+/***/ 7037:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 954, 23))
+
+/***/ })
+
+};
+;
